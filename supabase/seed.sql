f9ab5004f76f991f2c86dbe5a92878e32bc9c4e4
SavviSam <swghazzawi@gmail.com>
2024-12-06 17:18:53 -0700

    add seed data

---
+++ supabase/seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO categories (name) VALUES
+  ('Breads'),
+  ('Pastries'),
+  ('Sandwiches'),
+  ('Beverages');
+
+INSERT INTO menu_items (name, description, price, category_id, image_url) VALUES
+  ('Sourdough Loaf', 'Traditional San Francisco style sourdough', 8.99, 1, 'https://unsplash.com/photos/burger-on-white-ceramic-plate-XOv2hyvyLmE'),
+  ('Croissant', 'Buttery and flaky French pastry', 4.99, 2,),
+  ('Turkey Club', 'Turkey, bacon, lettuce, tomato on sourdough', 12.99, 3,),
+  ('Coffee', 'House blend coffee', 3.99, 4,);
+
+      
