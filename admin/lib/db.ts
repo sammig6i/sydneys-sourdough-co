d5ebdd97c939fd713a6b2a77586c3efea150374b
SavviSam <swghazzawi@gmail.com>
2025-01-05 02:18:11 -0700

    update todo

--- admin/lib/db.ts
+++ admin/lib/db.ts
@@ -1,5 +1,5 @@
 import 'server-only';
-// TODO replace with Supabase DB instead
+// TODO replace with Supabase DB operations instead
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import {
